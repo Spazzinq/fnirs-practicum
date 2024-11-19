7e4b9b786fb2f108d47c8d4e1306df17152b9169
George <26337405+Spazzinq@users.noreply.github.com>
2024-11-19 12:29:35 -0500

    Update main mvpa

--- main_mvpa.m
+++ main_mvpa.m
@@ -1,14 +1,23 @@
-ages = [6]; %[6, 24, 36, 60];
+ages = [6, 24, 36, 60]; %[6, 24, 36, 60];
 
 %% Filter incl_channel
 % Will take the MCP_struct and return front_MCP_Chan, which contains only
 % the "frontal" channels for each age condition (Based on Emberson et al.
 % 2017, as well as Fu & Richards 2023)
-incl_channels = [1,2,3,4,5,6,7,8,9,10,11,14];
-incl_features = zeros(2,32);
-incl_features(:,incl_channels) = 1;
-channels = find(incl_features(:))
+incl_channels_6mo = [12, 13, 14, 15, 28, 29, 30, 31];
+incl_channels_older = [15, 16, 17, 18, 34, 35, 36, 37];
 
 for i = 1:length(ages)
+    if ages(i) == 6
+        incl_channels = incl_channels_6mo;
+    else
+        incl_channels = incl_channels_older;
+    end
+
+    incl_features = zeros(2,32);
+    incl_features(:,incl_channels) = 1;
+    channels = find(incl_features(:))
+
+
     mvpa(ages(i), channels, 'ppPracF24') % the suffix for the preprocessed files
 end
